9d622fa0f0296acf4c3ffd740c184fba55f91eb7
Chris Chares <chrisc@activewebsite.com>
2014-03-26 16:19:51 -0600

    Initial Commit

---
+++ fontAwesomeTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fontAwesomeTest
+//
+//  Created by Chris Chares on 3/26/14.
+//  Copyright (c) 2014 Eunoia Design Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ fontAwesomeTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fontAwesomeTest
+//
+//  Created by Chris Chares on 3/26/14.
+//  Copyright (c) 2014 Eunoia Design Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
